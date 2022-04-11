f3b051245811d73839c9b58fa78673bbcdc905e6
Bruna Conrado <100372472+buhkellen@users.noreply.github.com>
2022-04-11 18:16:51 -0300

    Create pneu.cs

---
+++ pneu.cs
@@ -0,0 +1,11 @@
+using System;
+
+class TESTE {
+    static void Main () {
+        var N = int.Parse(Console.ReadLine());
+        var M = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(N-M);
+        
+    }
+}
